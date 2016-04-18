ed9a14f3edbe82d2566fdcd1a7293c198e736b30
mindyyuan <mindyuan@gmail.com>
2016-04-18 19:52:31 -0400

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,20 @@
-# Implement your object-oriented solution here!
+class SmallestMultiple
+  attr_accessor :num
+  def initialize(num)
+    @num = num
+  end
+
+  def lcm
+    product = num
+    factor = 1
+      while factor <= num
+        if product % factor == 0
+          factor += 1
+        else
+          factor = 1
+          product += 1
+        end
+      end
+    product
+  end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,13 @@
-# Implement your procedural solution here!
+def smallest_multiple(num)
+  product = num
+  factor = 1
+    while factor <= num
+      if product % factor == 0
+        factor += 1
+      else
+        factor = 1
+        product += 1
+      end
+    end
+  product
+end
